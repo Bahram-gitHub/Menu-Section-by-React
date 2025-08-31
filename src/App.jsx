0eb46f2896de483b277c3226ed0622054b72552f
bta62 <bta62@sfu.ca>
2025-08-31 14:23:26 -0700

    categories button for each category in data showmn in the top

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import Title from './Title';
 import { useState } from 'react';
 import menu from './data';
 import Menu from './Menu';
+import Title from './Title';
+import Categories from './Categories';
+
+const allCategories = [
+  'all',
+  ...new Set(
+    menu.map((item) => (
+      item.category
+    ))
+  ),
+];
 
 const App = () => {
-  const[menuItems, setMenuItems] = useState(menu)
-
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu"></Title>
+        <Categories categories={categories} />
         <Menu items={menuItems}></Menu>
       </section>
     </main>
