8667322213cf0cfd519c3edd8879b6d70ee17597
bta62 <bta62@sfu.ca>
2025-08-31 21:09:25 -0700

    the category button is functional right now

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,20 @@ const allCategories = [
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category)=>{
+    if (category === 'all'){
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item)=> item.category === category);
+    setMenuItems(newItems)
+  }
+  
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu"></Title>
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems}/>
         <Menu items={menuItems}></Menu>
       </section>
     </main>
